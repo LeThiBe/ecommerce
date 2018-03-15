9e996dfa406f99c14fa13356aac7cafbaeec9f51
quangnk234 <nguyenkhoaquang501@gmail.com>
2018-03-15 14:52:25 +0700

    sign in fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
-User.create!(name:  "Admin", email: "quangnkc2@gmail.com",
+User.create!(name:  "Quang", email: "quangnkc2@gmail.com",
   password: "123456", password_confirmation: "123456",
-  is_admin: true, status: true, phone: "0123456789", address: "Da Nang", point: 0, avatar: "avt1.jpg")
+  is_admin: false, activated: true, phone: "0123456789", address: "Da Nang", point: 0)
 
-10.times do |n|
+9.times do |n|
 name  = Faker::Name.name
 email = "example#{n+1}@gmail.com"
-avatar = "avt2.jpg"
 User.create!(name:  name, email: email,
   password: "123456", password_confirmation: "123456",
-  is_admin: false, status: true, phone: "0123456789", address: "Da Nang", point: 0, avatar: avatar)
+  is_admin: false, activated: true, phone: "0123456789", address: "Da Nang", point: 0)
 end
